8a4de5ba79b9ce9f8a03d33b5e45c14a11c3a86a
ThomasMonnier <thomas.monnier@papernest.com>
2022-11-10 12:00:35 +0100

    [FIX] button

--- app.py
+++ app.py
@@ -5,19 +5,13 @@ from src.utils import ocr_tesseract, convert_pdf
 
 
 def classifier_country(file):
-    _, lng_api, _ = st.columns(3)
-
-    with lng_api:
-        lng_api_button = st.button('Get the language of file', key="clf_1")
-
-    if lng_api_button:
-        img_path = convert_pdf(file)
-        ocr_str = ocr_tesseract(img_path)
-        all_lng, lng = classifier_country(ocr_str)
-        if lng is None:
-            st.error('Impossible to detect the language of the document, check {}'.format(all_lng))
-        else:
-            st.info('Country: {}'.format(lng))
+    img_path = convert_pdf(file)
+    ocr_str = ocr_tesseract(img_path)
+    all_lng, lng = classifier_country(ocr_str)
+    if lng is None:
+        st.error('Impossible to detect the language of the document, check {}'.format(all_lng))
+    else:
+        st.info('Country: {}'.format(lng))
 
 
 def classifier_supplier():
@@ -44,4 +38,11 @@ if __name__=="__main__":
     if uploaded_file:
         with open(uploaded_file.name, "wb") as buffer:
             shutil.copyfileobj(uploaded_file, buffer)
-        classifier_country(uploaded_file.name)
+        
+
+        _, lng_api, _ = st.columns(3)
+        with lng_api:
+            lng_api_button = st.button('Get the language of file', key="clf_1")
+        
+        if lng_api_button:
+            classifier_country(uploaded_file.name)
