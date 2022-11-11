65b999b80c871faf664c96d889a1c5cab1bdbc7f
ThomasMonnier <thomas.monnier@papernest.com>
2022-11-11 12:15:05 +0100

    [FIX] dimension

--- src/clf_supplier.py
+++ src/clf_supplier.py
@@ -5,7 +5,7 @@ def prepare_img(img_path, width=512, height=512):
     img = cv2.imread(img_path)
     resized_img = cv2.resize(img, (width, height))
     nx, ny, nz = np.array(resized_img).shape
-    img_final = np.array(resized_img).reshape((nx * ny * nz))
+    img_final = np.array(resized_img).reshape(1 * (nx * ny * nz))
     return img_final
 
 
